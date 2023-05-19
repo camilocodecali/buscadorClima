e1376cb10ba4bd48df34c3cf1851429e2b5c0ec6
Camilo Rodriguez <camilorodriguezco7@gmail.com>
2023-05-19 01:13:40 -0500

    mosrtando clima actual, maximo y minimo mostrando con estilos

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -9,11 +9,14 @@ const Resultado = () => {
     const kelvin = 273.15
 
   return (
-    <div className="contenedor">
+    <div className="contenedor clima">
       <h2>El clima de {name} es: </h2>
-      <p>Temperatura actual: {parseInt(main.temp - kelvin)} <span>&#x2103;</span></p>
-      <p>Temperatura mínima: {parseInt(main.temp - kelvin)} <span>&#x2103;</span></p>
-      <p>Temperatura máxima: {parseInt(main.temp - kelvin)} <span>&#x2103;</span></p>
+      <p>{parseInt(main.temp - kelvin)} <span>&#x2103;</span></p>
+      <div className="temp_min_max">
+        <p>Mín: {parseInt(main.temp_min - kelvin)} <span>&#x2103;</span></p>
+        <p>Máx: {parseInt(main.temp_max - kelvin)} <span>&#x2103;</span></p>
+
+      </div>
 
     </div>
   )
