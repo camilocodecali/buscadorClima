43f26ab4cd8080ec3fa0404f3cfffb70138307f4
Camilo Rodriguez <camilorodriguezco7@gmail.com>
2023-05-19 00:38:35 -0500

    first commit formulario ya almacenando datos y enviandolo al provider

---
+++ src/components/AppClima.jsx
@@ -0,0 +1,13 @@
+import Formulario from "./Formulario"
+
+const AppClima = () => {
+  return (
+    <>
+        <main className="dos-columnas">
+            <Formulario></Formulario>
+        </main>
+    </>
+  )
+}
+
+export default AppClima
